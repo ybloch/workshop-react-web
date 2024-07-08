41c287a3bfec7df6008b6cbddb1bba5700426edc
yinonb11@gmail.com <yinonb11@gmail.com>
2024-07-08 10:00:03 +0300

    Use mongo atlas

--- backend/main.py
+++ backend/main.py
@@ -3,10 +3,7 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
-from dotenv import dotenv_values
 from routes import router as task_router
-
-dotenv_values(".env")
 
 app = FastAPI()
 app.add_middleware(
@@ -20,11 +17,7 @@ app.add_middleware(
 
 @app.on_event("startup")
 def startup_db_client():
-    app.mongodb_client = MongoClient(
-        os.getenv("MONGO_IP"),
-        username=os.getenv("MONGO_USER"),
-        password=os.getenv("MONGO_PASSWORD"),
-    )
+    app.mongodb_client = MongoClient("Link (Connection String) to your MongoDB")
     app.database = app.mongodb_client["bootcamp"]
 
 
